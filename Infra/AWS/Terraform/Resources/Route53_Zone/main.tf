519f6591b324512d490c4fb9accbd8a1fa0183fb
bdodlo123 <basavarajudodlo@gmail.com>
2023-12-23 19:09:00 +0530

    added correct working dir path

--- Infra/AWS/Terraform/Resources/Route53_Zone/main.tf
+++ Infra/AWS/Terraform/Resources/Route53_Zone/main.tf
@@ -1,7 +1,5 @@
 # Route53_Zone will be created#
-resource "aws_route53_zone" "zone" {
-#checkov:skip=CKV2_AWS_39:Ensure Domain Name System (DNS) query logging is enabled for Amazon Route 53 hosted zones
-#checkov:skip=CKV2_AWS_38:Ensure Domain Name System Security Extensions (DNSSEC) signing is enabled for Amazon Route 53 public 
+resource "aws_route53_zone" "zone" { 
   name = var.zone_name
   tags = var.tags
 }
